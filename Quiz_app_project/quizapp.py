dc2643dba0d4a8d56d84d6426919791c8e39d1e0
Sohan Shrestha <itsmesohan456@gmail.com>
2026-02-24 20:43:08 +0545

    Modified some minor errors in displaying correct answers

--- Quiz_app_project/quizapp.py
+++ Quiz_app_project/quizapp.py
@@ -15,27 +15,27 @@ def run_quiz():
         {
             "prompt": "Which programming language is known as the snake langauge?",
             "options": ["A) Java", "B) C++" "C) Python", "D) Ruby"],
-            "answer": "C)"
+            "answer": "C"
         },
         {
             "prompt": "Which planet in the solar system is known as the 'Red Planet'?",
             "options": ["A) Mars", "B) Venus", "C) Jupiter", "D) Pluto"],
-            "answer": "A)"
+            "answer": "A"
         },
         {
             "prompt": "Which country's flag is the only non-rectangular flag in the world?",
             "options": ["A) USA", "B) Switzerland" "C) Nepal" "D) Vatican City"],
-            "answer": "C)"
+            "answer": "C"
         },
         {
             "prompt": "Who created the python programming language?",
             "options": ["A) Guido Van Rossum", "B) Dennis Ritchie", "C) Tim Berners-Lee", "D) Linus Torvalds"],
-            "answer": "A)"
+            "answer": "A"
         },
         {
             "prompt": "The 2026 FIFA world cup will be the first to be hosted by three different countrires. Which ones are they?",
             "options": ["A) Brazil, Argentina, Uruguay", "B) Spain, Portugal and Morocco", "C) China, Japan and South Korea", "D) USA, Canada and Mexico"],
-            "answer": "D)"
+            "answer": "D"
         }
 ]
     random.shuffle(questions)
